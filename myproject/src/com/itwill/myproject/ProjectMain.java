9d0a48f6ac49e0b7afb1dc8a3efa0d2ccca2fd39
goodLuck <hasin927@gmail.com>
2024-11-25 14:21:02 +0900

    커밋 WILL

--- myproject/src/com/itwill/myproject/ProjectMain.java
+++ myproject/src/com/itwill/myproject/ProjectMain.java
@@ -6,7 +6,7 @@ public class ProjectMain {
 		System.out.println("Hello, GitHub!");
 		System.out.println("안녕하세요, 깃허브!");
 		System.out.println("깃허브에서 작성됨.");
-		System.out.println("1");
+		System.out.println("1 WILL");
 		System.out.println("2");
 	}
 
