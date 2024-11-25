ced129cde9e7ec031604848af6fca003f3c5c402
goodLuck <hasin927@gmail.com>
2024-11-25 14:31:44 +0900

    로컬2에서 2수정

--- myproject/src/com/itwill/myproject/ProjectMain.java
+++ myproject/src/com/itwill/myproject/ProjectMain.java
@@ -7,7 +7,7 @@ public class ProjectMain {
 		System.out.println("안녕하세요, 깃허브!");
 		System.out.println("깃허브에서 작성됨.");
 		System.out.println("1 WILL");
-		System.out.println("2");
+		System.out.println("2 로컬2에서 수정함");
 	}
 
 }
