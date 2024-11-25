baf68347f78d9eb404989c6434c654f6c3503a29
goodLuck <hasin927@gmail.com>
2024-11-25 14:12:22 +0900

    local1에서 커밋.

--- myproject/src/com/itwill/myproject/ProjectMain.java
+++ myproject/src/com/itwill/myproject/ProjectMain.java
@@ -3,7 +3,7 @@ package com.itwill.myproject;
 public class ProjectMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, Git!");
+		System.out.println("Hello, GitHub!");
 		System.out.println("안녕하세요, 깃허브!");
 		System.out.println("깃허브에서 작성됨.");
 	}
